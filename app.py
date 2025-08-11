c4063c93b3296578ec8ecab7ddd54bbe1f6dae6e
cyrilmartin08 <152998847+cyrilmartin08@users.noreply.github.com>
2025-08-11 16:28:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -191,5 +191,5 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-if __name__ == "__main__":
-    app.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 5000)))
+# if __name__ == "__main__":
+#    app.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 5000)))
